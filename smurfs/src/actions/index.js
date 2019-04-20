9c9245a3d30ced89ea77dd458524687c0c3cbaeb
RaneWallin <rane@ranewallin.com>
2019-04-19 18:36:06 -0700

    getSmurf action creator

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,18 @@
-/* 
+import axios from 'axios';
+
+/*
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const START_FETCH = "START_FETCH";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
+export const GET_SMURFS = "GET_SMURFS";
+export const ADD_SMURF = "ADD_SMURF";
+export const UPDATE_SMURF = "UPDATE_SMURF";
+export const DELETE_SMURF = "DELETE_SMURF";
+
+const SMURFY_API = "http://localhost:3333";
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +24,22 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = _ => dispatch => {
+    dispatch({ type: START_FETCH });
+
+    axios
+        .get(SMURFY_API)
+        .then(res =>
+            dispatch({
+                type: GET_SMURFS,
+                payload: res.data
+            })
+        )
+        .catch(err =>
+            dispatch({
+                type: FETCH_FAIL,
+                payload: err
+            })
+        )
+}
